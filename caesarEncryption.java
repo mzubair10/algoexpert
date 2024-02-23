58f8d4fabad0f85068e1d15d05184fdcc09f9e5c
Mohamed Zubair <mohamed.zubairrazak@freshworks.com>
2024-02-23 11:08:02 +0530

    encryption

---
+++ caesarEncryption.java
@@ -0,0 +1,21 @@
+class Program {
+    public static String caesarCypherEncryptor(String str, int key) {
+        key = key % 26;
+        char[] chArray = str.toCharArray();
+        String output = "";
+        for (int iter = 0; iter < chArray.length; iter++) {
+            int unicodeRep = (int) chArray[iter];
+            unicodeRep = unicodeRep + key;
+            if (unicodeRep > 122) {
+                int difference = unicodeRep - 122;
+                unicodeRep = 96 + difference;
+            }
+            output = (new StringBuilder()).append(output).append(Character.toString(unicodeRep)).toString();
+        }
+        return output;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(caesarCypherEncryptor("abc", 5));
+    }
+}
